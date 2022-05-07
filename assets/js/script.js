8bfaf5573ca5a839fc001e02918d16b2f36b5ffe
AJ Jaswal <jaswalakshay@yahoo.com>
2022-05-07 18:35:54 -0400

    html tweaks and js vars

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var APIkey = "16db7d4e91ab780f9f9778cedb8d685f";
+
+var cityinput = $('#city-input');
+var searchBtn = $('#search-button');
+var clearBtn = $('#clear-button');
+var pastsearches = $('#past-searches');
+
+var currentCity;
+
