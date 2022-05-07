38398cbc57790218cf6460c2b32523f5e791de22
AJ Jaswal <jaswalakshay@yahoo.com>
2022-05-07 18:36:23 -0400

    call API

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,29 @@ var pastsearches = $('#past-searches');
 
 var currentCity;
 
+// call openweather api to get data
+function getWeather(data) {
+
+    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIkey}`
+    fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            // Current weather, city name, and date
+            var currentConditionsEl = $('#currentConditions');
+            currentConditionsEl.addClass('border border-primary');
+
+           
+            var cityNameEl = $('<h2>');
+            cityNameEl.text(currentCity);
+            currentConditionsEl.append(cityNameEl);
+            
+           
+            var currentCityDate = data.current.dt;
+            currentCityDate = moment.unix(currentCityDate).format("MM/DD/YYYY");
+            var currentDateEl = $('<span>');
+            currentDateEl.text(` (${currentCityDate}) `);
+            cityNameEl.append(currentDateEl);
+
